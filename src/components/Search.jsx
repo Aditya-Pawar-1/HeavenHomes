4047b34f16f77c523938f45e175b48cd8572d6b2
Aditya <pawaraditya200410@gmail.com>
2024-03-29 22:07:11 +0530

    images url

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,14 +3,16 @@ import SearchBox from './SearchBox'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import SearchCards from './SearchCards'
+import i from '../assets/images/Img1.png';
 
 const Search = () => {
   const [images, setImages] = useState([
-    "/assets/images/Img1.png",
-    "/assets/images/Img1.png",
-    "/assets/images/Img1.png",
+    "/HeavenHomes/src/assets/images/Img1.png",
+    "/HeavenHomes/src/assets/images/Img1.png",
+    "/HeavenHomes/src/assets/images/Img1.png",
   ])
 
+  console.log({i})
   return (
     <div className='w-full h-fit'>
       <Navbar />
