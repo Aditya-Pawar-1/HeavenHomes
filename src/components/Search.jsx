1fea981b82576109679b0c3502e568f041707da8
Aditya <pawaraditya200410@gmail.com>
2024-04-06 17:13:13 +0530

    images

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,9 +6,9 @@ import SearchCards from './SearchCards'
 
 const Search = () => {
   const [images, setImages] = useState([
-    "Img2.jpg",
-    "Img2.jpg",
-    "Img2.jpg",
+    "/HeavenHomes/public/Img2.jpg",
+    "/HeavenHomes/public/Img2.jpg",
+    "/HeavenHomes/public/Img2.jpg",
   ])
 
   return (
